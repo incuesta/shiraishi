3754af8ed8faff86539e36965ada555b7384d8a3
incuesta <incuesta8@gmail.com>
2018-02-25 18:43:54 +0800

    Added Article Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
 	  
 
-	  root 'pages#home'
+
+	# Article
+  	resources :articles
+	
 
 
-	  # Static Pages
-	  get 'pages/services'
-	  get 'pages/articles'
-	  get 'pages/organization'
-	  get 'pages/about'
-	  get 'pages/contacts'
-	  get 'pages/help'
+	# Static Pages
+	get 'pages/services'
+	get 'pages/articles'
+	get 'pages/organization'
+	get 'pages/about'
+	get 'pages/contacts'
+	get 'pages/help'
 
 
+	# Root
+	root 'pages#home'
 end
