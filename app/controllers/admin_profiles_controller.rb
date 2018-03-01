9cfb33b3e3df1a96426606047781082e41abf96e
incuesta <incuesta8@gmail.com>
2018-03-01 17:54:45 +0800

    Finished Implementing Pundit, Shyshyshy!

--- app/controllers/admin_profiles_controller.rb
+++ app/controllers/admin_profiles_controller.rb
@@ -1,5 +1,4 @@
 class AdminProfilesController < ApplicationController
-  before_action :authenticate_admin!
   before_action :set_admin_profile, only: [:show, :edit, :update, :destroy]
   
   
@@ -9,12 +8,14 @@ class AdminProfilesController < ApplicationController
   # GET /admin_profiles.json
   def index
     @admin_profiles = AdminProfile.all
+
+    authorize AdminProfile
   end
 
   # GET /admin_profiles/1
   # GET /admin_profiles/1.json
   def show
-
+    authorize @admin_profile
   end
 
 
@@ -25,6 +26,9 @@ class AdminProfilesController < ApplicationController
     # If you use build it will delete your profile because of 1:1 relationship
     @admin_profile = AdminProfile.new
     
+
+    authorize @admin_profile
+
     # Redirect to update if there's already a Profile made.
     redirect_to edit_admin_profile_path(current_admin.admin_profile) if current_admin.admin_profile
 
@@ -34,6 +38,10 @@ class AdminProfilesController < ApplicationController
   # POST /admin_profiles.json
   def create
     @admin_profile = current_admin.build_admin_profile(admin_profile_params)
+
+
+    authorize @admin_profile
+
 
     respond_to do |format|
       if @admin_profile.save
@@ -51,20 +59,23 @@ class AdminProfilesController < ApplicationController
 
    # GET /admin_profiles/1/edit
   def edit
+      authorize @admin_profile
   end
 
   # PATCH/PUT /admin_profiles/1
   # PATCH/PUT /admin_profiles/1.json
   def update
-    respond_to do |format|
-      if @admin_profile.update(admin_profile_params)
-        format.html { redirect_to @admin_profile, notice: 'Admin profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin_profile }
-      else
-        format.html { render :edit }
-        format.json { render json: @admin_profile.errors, status: :unprocessable_entity }
+      authorize @admin_profile
+
+      respond_to do |format|
+        if @admin_profile.update(admin_profile_params)
+          format.html { redirect_to @admin_profile, notice: 'Admin profile was successfully updated.' }
+          format.json { render :show, status: :ok, location: @admin_profile }
+        else
+          format.html { render :edit }
+          format.json { render json: @admin_profile.errors, status: :unprocessable_entity }
+        end
       end
-    end
   end
 
 
@@ -73,6 +84,8 @@ class AdminProfilesController < ApplicationController
   # DELETE /admin_profiles/1
   # DELETE /admin_profiles/1.json
   def destroy
+    authorize @admin_profile
+    
     @admin_profile.destroy
 
     respond_to do |format|
---
+++ app/policies/client_profile_policy.rb
@@ -0,0 +1,57 @@
+class ClientProfilePolicy < ApplicationPolicy
+  	
+
+	def index?
+		loan_officers_only
+	end
+
+
+	# Allow only Client to update his OWN
+	def show?
+		(@user.instance_of?(Client) && client_profile.client == @user) || loan_officers_only
+	end
+
+
+
+
+	def new?
+		@user.instance_of?(Client) || loan_officers_only
+	end
+
+	def create?
+		@user.instance_of?(Client) || loan_officers_only
+	end
+
+
+
+
+	# Allow only Client to update his OWN
+	def edit?
+		(@user.instance_of?(Client) && client_profile.client == @user) || loan_officers_only
+	end
+
+	# Allow only Client to update his OWN
+	def update?
+		(@user.instance_of?(Client) && client_profile.client == @user) || loan_officers_only
+	end
+
+
+
+
+	# Allow only Client to update his OWN
+	def destroy?
+		(@user.instance_of?(Client) && client_profile.client == @user) || @user.instance_of?(Admin)	
+	end
+
+
+
+  	private
+
+  		def client_profile
+  			record
+  		end
+
+  		def loan_officers_only
+  			@user.instance_of?(Admin) || @user.instance_of?(Accountant) || @user.instance_of?(LoanManager)
+  		end
+end
