8aa096b72900df2b08be95f2855ba8a46486a6b7
incuesta <incuesta8@gmail.com>
2018-04-16 18:50:53 +0800

    f

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   		# Include default devise modules. Others available are:
  		# :confirmable, :lockable, :timeoutable and :omniauthable
   		devise :database_authenticatable, :registerable,
-         	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :session_limitable
+         	:recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
 
 
     	# Admin (1:1) AdminProfile
